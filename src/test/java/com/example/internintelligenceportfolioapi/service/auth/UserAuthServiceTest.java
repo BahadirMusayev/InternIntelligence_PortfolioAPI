dcb871d7173b865e63bcd0b2df593895845033a8
Bahadir-coder <bahadurmusayev15@gmail.com>
2025-04-13 19:50:22 +0400

    Portfolio API

--- src/test/java/com/example/internintelligenceportfolioapi/service/auth/UserAuthServiceTest.java
+++ src/test/java/com/example/internintelligenceportfolioapi/service/auth/UserAuthServiceTest.java
@@ -10,10 +10,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -122,5 +126,30 @@ class UserAuthServiceTest {
         verify(authManager).authenticate(any(UsernamePasswordAuthenticationToken.class));
         verify(jwtService).generateToken(userEntity);
         verifyNoMoreInteractions(userRepository, passwordEncoder, authManager, jwtService);
+    }
+
+    @Test
+    void getUser_ShouldReturnUserEntity_WhenAuthenticated() {
+        UserEntity mockUser = new UserEntity();
+        mockUser.setId(1);
+        mockUser.setEmail("test@example.com");
+
+        Authentication authentication = mock(Authentication.class);
+        SecurityContext securityContext = mock(SecurityContext.class);
+
+        when(authentication.getPrincipal()).thenReturn(mockUser);
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+
+        try (MockedStatic<SecurityContextHolder> mockedHolder = Mockito.mockStatic(SecurityContextHolder.class)) {
+            mockedHolder.when(SecurityContextHolder::getContext).thenReturn(securityContext);
+
+            UserEntity result = UserAuthService.getUser();
+
+            assertNotNull(result, "User is null !");
+            assertEquals("test@example.com", result.getEmail(), "User is not found !");
+
+            verify(authentication).getPrincipal();
+            verify(securityContext).getAuthentication();
+        }
     }
 }
