56438c1eef31b676d92a96c1a43ab5a4aabf7d48
Bahadir-coder <bahadurmusayev15@gmail.com>
2025-04-13 20:26:07 +0400

    Portfolio API

--- src/test/java/com/example/internintelligenceportfolioapi/service/EducationServiceTest.java
+++ src/test/java/com/example/internintelligenceportfolioapi/service/EducationServiceTest.java
@@ -1,12 +1,74 @@
 package com.example.internintelligenceportfolioapi.service;
 
+import com.example.internintelligenceportfolioapi.dao.entity.EducationEntity;
+import com.example.internintelligenceportfolioapi.dao.entity.UserEntity;
+import com.example.internintelligenceportfolioapi.dao.repository.EducationRepository;
+import com.example.internintelligenceportfolioapi.mapper.EducationMapper;
+import com.example.internintelligenceportfolioapi.model.output.EducationDtoOutput;
+import com.example.internintelligenceportfolioapi.service.auth.UserAuthService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class EducationServiceTest {
+    @Mock
+    private EducationRepository educationRepository;
+
+    @Mock
+    private EducationMapper educationMapper;
+
+    @InjectMocks
+    private EducationService educationService;
 
     @Test
     void get() {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setId(1);
+
+        EducationEntity educationEntity = new EducationEntity();
+        educationEntity.setId(1);
+        educationEntity.setName("Compar");
+        educationEntity.setSpeciality("Java Backend Development");
+        educationEntity.setDegree("Advanced");
+        educationEntity.setStartDate(LocalDate.of(2024,7,1));
+        educationEntity.setEndDate(LocalDate.of(2024,12,1));
+        List<EducationEntity> educationEntities = List.of(educationEntity);
+        userEntity.setEducationEntities(educationEntities);
+
+        EducationDtoOutput educationDtoOutput = new EducationDtoOutput();
+        educationDtoOutput.setName("Compar");
+        educationDtoOutput.setSpeciality("Java Backend Development");
+        educationDtoOutput.setDegree("Advanced");
+        educationDtoOutput.setStartDate(LocalDate.of(2024,7,1));
+        educationDtoOutput.setEndDate(LocalDate.of(2024,12,1));
+        List<EducationDtoOutput> educationDtoOutputs = List.of(educationDtoOutput);
+
+        try(MockedStatic<UserAuthService> mockedStatic = Mockito.mockStatic(UserAuthService.class)){
+            mockedStatic.when(UserAuthService::getUser).thenReturn(userEntity);
+            when(educationMapper.mapEntityToDtoOutputs(educationEntities)).thenReturn(educationDtoOutputs);
+
+            List<EducationDtoOutput> result = educationService.get();
+
+            assertEquals(result.get(0).getName(), educationEntities.get(0).getName(), "Should change name !");
+            assertEquals(result.get(0).getSpeciality(), educationEntities.get(0).getSpeciality(), "Should change speciality !");
+            assertEquals(result.get(0).getDegree(), educationEntities.get(0).getDegree(), "Should change degree !");
+            assertEquals(result.get(0).getStartDate(), educationEntities.get(0).getStartDate(), "Should change start date !");
+            assertEquals(result.get(0).getEndDate(), educationEntities.get(0).getEndDate(), "Should change end date !");
+
+            mockedStatic.verify(UserAuthService::getUser);
+            verify(educationMapper).mapEntityToDtoOutputs(educationEntities);
+        }
     }
 }
