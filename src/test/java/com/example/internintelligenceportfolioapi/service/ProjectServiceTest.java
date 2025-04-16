a632446029c23581301d0016c69a4834a2733c0a
Bahadir-coder <bahadurmusayev15@gmail.com>
2025-04-16 21:55:03 +0400

    Portfolio API

--- src/test/java/com/example/internintelligenceportfolioapi/service/ProjectServiceTest.java
+++ src/test/java/com/example/internintelligenceportfolioapi/service/ProjectServiceTest.java
@@ -1,12 +1,10 @@
 package com.example.internintelligenceportfolioapi.service;
 
-import com.example.internintelligenceportfolioapi.dao.entity.EducationEntity;
 import com.example.internintelligenceportfolioapi.dao.entity.ProjectEntity;
 import com.example.internintelligenceportfolioapi.dao.entity.UserEntity;
 import com.example.internintelligenceportfolioapi.dao.repository.ProjectRepository;
 import com.example.internintelligenceportfolioapi.mapper.ProjectMapper;
-import com.example.internintelligenceportfolioapi.model.input.EducationDtoInput;
-import com.example.internintelligenceportfolioapi.model.output.EducationDtoOutput;
+import com.example.internintelligenceportfolioapi.model.input.ProjectDtoInput;
 import com.example.internintelligenceportfolioapi.model.output.ProjectDtoOutput;
 import com.example.internintelligenceportfolioapi.service.auth.UserAuthService;
 import org.junit.jupiter.api.Test;
@@ -44,7 +42,7 @@ class ProjectServiceTest {
         ProjectEntity projectEntity = new ProjectEntity();
         projectEntity.setId(1);
         projectEntity.setName("Reserone");
-        projectEntity.setUrl("http:localhost:8080/reserone");
+        projectEntity.setUrl("reserone.az");
         projectEntity.setAbout("Barber Reservation Project");
         projectEntity.setStartDate(LocalDate.of(2024, 7, 1));
         projectEntity.setEndDate(LocalDate.of(2024, 12, 1));
@@ -53,7 +51,7 @@ class ProjectServiceTest {
 
         ProjectDtoOutput projectDtoOutput = new ProjectDtoOutput();
         projectDtoOutput.setName("Reserone");
-        projectDtoOutput.setUrl("http:localhost:8080/reserone");
+        projectDtoOutput.setUrl("reserone.az");
         projectDtoOutput.setAbout("Barber Reservation Project");
         projectDtoOutput.setStartDate(LocalDate.of(2024, 7, 1));
         projectDtoOutput.setEndDate(LocalDate.of(2024, 12, 1));
@@ -76,46 +74,46 @@ class ProjectServiceTest {
         }
     }
 
-//    @Test
-//    @Transactional
-//    void add() {
-//        UserEntity userEntity = new UserEntity();
-//        userEntity.setId(1);
-//
-//        EducationDtoInput educationDtoInput = new EducationDtoInput();
-//        educationDtoInput.setName("Compar");
-//        educationDtoInput.setSpeciality("Java Backend Development");
-//        educationDtoInput.setDegree("Advanced");
-//        educationDtoInput.setStartDate(LocalDate.of(2024, 7, 1));
-//        educationDtoInput.setEndDate(LocalDate.of(2024, 12, 1));
-//
-//        EducationEntity educationEntity = new EducationEntity();
-//        educationEntity.setId(1);
-//        educationEntity.setName("Compar");
-//        educationEntity.setSpeciality("Java Backend Development");
-//        educationEntity.setDegree("Advanced");
-//        educationEntity.setStartDate(LocalDate.of(2024, 7, 1));
-//        educationEntity.setEndDate(LocalDate.of(2024, 12, 1));
-//
-//        List<EducationEntity> educationEntities = List.of(educationEntity);
-//        userEntity.setEducationEntities(educationEntities);
-//
-//        try (MockedStatic<UserAuthService> mockedStatic = Mockito.mockStatic(UserAuthService.class)) {
-//            mockedStatic.when(UserAuthService::getUser).thenReturn(userEntity);
-//            when(educationMapper.mapEducationDtoInputToEntity(educationDtoInput)).thenReturn(educationEntity);
-//
-//            educationService.add(educationDtoInput);
-//            EducationEntity actualEntity = userEntity.getEducationEntities().get(0);
-//
-//            assertEquals(educationDtoInput.getName(), actualEntity.getName(), "Should change name !");
-//            assertEquals(educationDtoInput.getSpeciality(), actualEntity.getSpeciality(), "Should change speciality !");
-//            assertEquals(educationDtoInput.getDegree(), actualEntity.getDegree(), "Should change degree !");
-//            assertEquals(educationDtoInput.getStartDate(), actualEntity.getStartDate(), "Should change start date !");
-//            assertEquals(educationDtoInput.getEndDate(), actualEntity.getEndDate(), "Should change end date !");
-//
-//            mockedStatic.verify(UserAuthService::getUser);
-//            verify(educationMapper).mapEducationDtoInputToEntity(educationDtoInput);
-//            verify(educationRepository).save(educationEntity);
-//        }
-//    }
+    @Test
+    @Transactional
+    void add() {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setId(1);
+
+        ProjectDtoInput projectDtoInput = new ProjectDtoInput();
+        projectDtoInput.setName("Reserone");
+        projectDtoInput.setUrl("reserone.az");
+        projectDtoInput.setAbout("Barber Reservation Project");
+        projectDtoInput.setStartDate(LocalDate.of(2024, 7, 1));
+        projectDtoInput.setEndDate(LocalDate.of(2024, 12, 1));
+
+        ProjectEntity projectEntity = new ProjectEntity();
+        projectEntity.setId(1);
+        projectEntity.setName("Reserone");
+        projectEntity.setUrl("reserone.az");
+        projectEntity.setAbout("Barber Reservation Project");
+        projectEntity.setStartDate(LocalDate.of(2024, 7, 1));
+        projectEntity.setEndDate(LocalDate.of(2024, 12, 1));
+
+        List<ProjectEntity> projectEntities = List.of(projectEntity);
+        userEntity.setProjectEntities(projectEntities);
+
+        try (MockedStatic<UserAuthService> mockedStatic = Mockito.mockStatic(UserAuthService.class)) {
+            mockedStatic.when(UserAuthService::getUser).thenReturn(userEntity);
+            when(projectMapper.mapProjectDtoInputToEntity(projectDtoInput)).thenReturn(projectEntity);
+
+            projectService.add(projectDtoInput);
+            ProjectEntity actualEntity = userEntity.getProjectEntities().get(0);
+
+            assertEquals(projectDtoInput.getName(), actualEntity.getName(), "Should change name !");
+            assertEquals(projectDtoInput.getUrl(), actualEntity.getUrl(), "Should change url !");
+            assertEquals(projectDtoInput.getAbout(), actualEntity.getAbout(), "Should change about !");
+            assertEquals(projectDtoInput.getStartDate(), actualEntity.getStartDate(), "Should change start date !");
+            assertEquals(projectDtoInput.getEndDate(), actualEntity.getEndDate(), "Should change end date !");
+
+            mockedStatic.verify(UserAuthService::getUser);
+            verify(projectMapper).mapProjectDtoInputToEntity(projectDtoInput);
+            verify(projectRepository).save(projectEntity);
+        }
+    }
 }
