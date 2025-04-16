292bfbfe75bb88312b317b34211ce46afce862df
Bahadir-coder <bahadurmusayev15@gmail.com>
2025-04-16 22:24:57 +0400

    Portfolio API

--- src/test/java/com/example/internintelligenceportfolioapi/service/ProjectServiceTest.java
+++ src/test/java/com/example/internintelligenceportfolioapi/service/ProjectServiceTest.java
@@ -116,4 +116,166 @@ class ProjectServiceTest {
             verify(projectRepository).save(projectEntity);
         }
     }
+
+    @Test
+    @Transactional
+    void updateName() {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setId(1);
+
+        ProjectEntity projectEntity = new ProjectEntity();
+        projectEntity.setId(1);
+        projectEntity.setName("ReserOne");
+
+        List<ProjectEntity> projectEntities = List.of(projectEntity);
+        userEntity.setProjectEntities(projectEntities);
+
+        try (MockedStatic<UserAuthService> mockedStatic = Mockito.mockStatic(UserAuthService.class)) {
+            mockedStatic.when(UserAuthService::getUser).thenReturn(userEntity);
+
+            String newName = "Reservendor";
+            projectService.updateName(0, newName);
+
+            assertNotNull(projectEntity.getName(), "Name is null !");
+            assertEquals(newName, projectEntity.getName(), "Should change name !");
+
+            mockedStatic.verify(UserAuthService::getUser);
+            verify(projectRepository).save(projectEntity);
+        }
+    }
+
+    @Test
+    @Transactional
+    void updateUrl() {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setId(1);
+
+        ProjectEntity projectEntity = new ProjectEntity();
+        projectEntity.setId(1);
+        projectEntity.setUrl("reserone.az");
+
+        List<ProjectEntity> projectEntities = List.of(projectEntity);
+        userEntity.setProjectEntities(projectEntities);
+
+        try (MockedStatic<UserAuthService> mockedStatic = Mockito.mockStatic(UserAuthService.class)) {
+            mockedStatic.when(UserAuthService::getUser).thenReturn(userEntity);
+
+            String newUrl = "reserone.com";
+            projectService.updateUrl(0, newUrl);
+
+            assertNotNull(projectEntity.getUrl(), "Url is null !");
+            assertEquals(newUrl, projectEntity.getUrl(), "Should change url !");
+
+            mockedStatic.verify(UserAuthService::getUser);
+            verify(projectRepository).save(projectEntity);
+        }
+    }
+
+    @Test
+    @Transactional
+    void updateAbout() {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setId(1);
+
+        ProjectEntity projectEntity = new ProjectEntity();
+        projectEntity.setId(1);
+        projectEntity.setAbout("Barber Reservation Project");
+
+        List<ProjectEntity> projectEntities = List.of(projectEntity);
+        userEntity.setProjectEntities(projectEntities);
+
+        try (MockedStatic<UserAuthService> mockedStatic = Mockito.mockStatic(UserAuthService.class)) {
+            mockedStatic.when(UserAuthService::getUser).thenReturn(userEntity);
+
+            String newAbout = "Barber Reservation";
+            projectService.updateAbout(0, newAbout);
+
+            assertNotNull(projectEntity.getAbout(), "About is null !");
+            assertEquals(newAbout, projectEntity.getAbout(), "Should change about !");
+
+            mockedStatic.verify(UserAuthService::getUser);
+            verify(projectRepository).save(projectEntity);
+        }
+    }
+
+    @Test
+    @Transactional
+    void updateStartDate() {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setId(1);
+
+        ProjectEntity projectEntity = new ProjectEntity();
+        projectEntity.setId(1);
+        projectEntity.setStartDate(LocalDate.of(1990,1,1));
+
+        List<ProjectEntity> projectEntities = List.of(projectEntity);
+        userEntity.setProjectEntities(projectEntities);
+
+        try (MockedStatic<UserAuthService> mockedStatic = Mockito.mockStatic(UserAuthService.class)) {
+            mockedStatic.when(UserAuthService::getUser).thenReturn(userEntity);
+
+            LocalDate newStartDate = LocalDate.of(1990,1,8);
+            projectService.updateStartDate(0, newStartDate);
+
+            assertNotNull(projectEntity.getStartDate(), "Start Date is null !");
+            assertEquals(newStartDate, projectEntity.getStartDate(), "Should change start date !");
+
+            mockedStatic.verify(UserAuthService::getUser);
+            verify(projectRepository).save(projectEntity);
+        }
+    }
+
+    @Test
+    @Transactional
+    void updateEndDate() {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setId(1);
+
+        ProjectEntity projectEntity = new ProjectEntity();
+        projectEntity.setId(1);
+        projectEntity.setEndDate(LocalDate.of(1990,1,1));
+
+        List<ProjectEntity> projectEntities = List.of(projectEntity);
+        userEntity.setProjectEntities(projectEntities);
+
+        try (MockedStatic<UserAuthService> mockedStatic = Mockito.mockStatic(UserAuthService.class)) {
+            mockedStatic.when(UserAuthService::getUser).thenReturn(userEntity);
+
+            LocalDate newEndDate = LocalDate.of(1990,1,8);
+            projectService.updateEndDate(0, newEndDate);
+
+            assertNotNull(projectEntity.getEndDate(), "End Date is null !");
+            assertEquals(newEndDate, projectEntity.getEndDate(), "Should change end date !");
+
+            mockedStatic.verify(UserAuthService::getUser);
+            verify(projectRepository).save(projectEntity);
+        }
+    }
+
+    @Test
+    @Transactional
+    void delete(){
+        UserEntity userEntity = new UserEntity();
+        userEntity.setId(1);
+
+        ProjectEntity projectEntity = new ProjectEntity();
+        projectEntity.setId(1);
+        projectEntity.setName("ReserOne");
+        projectEntity.setUrl("reserone.com");
+        projectEntity.setAbout("Barber Reservation Project");
+        projectEntity.setStartDate(LocalDate.of(2024, 7, 1));
+        projectEntity.setEndDate(LocalDate.of(2024, 12, 1));
+
+        List<ProjectEntity> projectEntities = List.of(projectEntity);
+        userEntity.setProjectEntities(projectEntities);
+
+        try (MockedStatic<UserAuthService> mockedStatic = Mockito.mockStatic(UserAuthService.class)) {
+            mockedStatic.when(UserAuthService::getUser).thenReturn(userEntity);
+
+            projectService.delete(0);
+
+            mockedStatic.verify(UserAuthService::getUser);
+            verify(projectRepository).deleteById(1);
+        }
+    }
 }
